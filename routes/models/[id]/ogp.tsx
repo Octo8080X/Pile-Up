9edb1e7db0a76097799587359e1890f8a00d0a6d
Octo8080 <okutannderuta0529@gmail.com>
2024-07-02 00:41:19 +0900

    update

--- routes/models/[id]/ogp.tsx
+++ routes/models/[id]/ogp.tsx
@@ -4,9 +4,12 @@ import { callCreateOgp } from "../../../utils/queues.ts";
 export const handler: Handlers = {
   async GET(_req, ctx) {
     try {
+      console.log("GET /models/:id/ogp");
       const img = await getModelOgpImage(ctx.params.id);
       return new Response(img);
     } catch (e) {
+      console.error(e);
+      callCreateOgp(ctx.params.id);
       return new Response("Not Found", { status: 404 });
     }
   },
