f625ca5876c354293324b025d3bb21b960ded4a5
Octo8080 <okutannderuta0529@gmail.com>
2024-07-01 23:56:13 +0900

    update queue

--- routes/models/[id]/ogp.tsx
+++ routes/models/[id]/ogp.tsx
@@ -1,11 +1,16 @@
 import { Handlers } from "$fresh/server.ts";
 import { getModelOgpImage } from "../../../utils/kvstorage.ts";
+import { callCreateOgp } from "../../../utils/queues.ts";
 export const handler: Handlers = {
   async GET(_req, ctx) {
     try {
       const img = await getModelOgpImage(ctx.params.id);
+      if(img === null) {
+        throw new Error("Not Found");
+      }
       return new Response(img);
     } catch (e) {
+      await callCreateOgp(ctx.params.id);
       return new Response("Not Found", { status: 404 });
     }
   },
